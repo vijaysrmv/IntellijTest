0a9e662bfb990358587e08faa6be623ab54d430f
vijaysrmv <vvsharma36@gmail.com>
2024-06-16 12:02:58 +0530

    Change in main.java File

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-       // System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println("Hi Vijay");
-            System.out.println("Hi");
-        }
+
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("Vijay");
+        a.add("Vinay");
+        a.add("Vishal");
+        System.out.println(a);
+
+        System.out.println("New One");
+        a.stream().filter(s -> s.startsWith("V")).forEach(System.out::println);
     }
 }
